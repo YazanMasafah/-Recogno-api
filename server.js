8da3b2cf103c2882976f16ca0a6f0d1db9deb656
YazanMasafah <yazan.masafah@gmail.com>
2020-05-20 23:54:55 +0300

    API v1

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt= require('bcrypt-nodejs');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 const database = {
     users: [
@@ -24,23 +28,61 @@ const database = {
 }
 
 app.get('/', (req, res) => {
-    res.send('this is working');
+    res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
-    res.json('signin');
+    if ((req.body.email === database.users[0].email) && (req.body.password === database.users[0].password)) {
+        res.json('success');
+     } else {
+        res.status(400).json('error logging in');
+     }
+})
+
+
+app.post('/register', (req, res)=> {
+const { email, name, password } = req.body;
+database.users.push({
+            id: '789',
+            name: name,
+            email: email,
+            password: password,
+            entries: 0,
+            joined: new Date()
+})
+res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+        })
+    if (!found) {
+        res.status(400).json('user not found');
+    }
+})
+
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+        })
+    if (!found) {
+        res.status(400).json('user not found');
+    }
 })
 
 app.listen(4000,()=> {
 console.log('app is running on port 4000');
 })
-
-/*
-res = this is working
-/signin --> pot = Success /Fail
-/register --> post = user
-/profile/userId --> get = user
-/image --> put --> user or count
-
-
-*/
